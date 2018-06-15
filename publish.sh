0187560a5168e1d7929432e5e86e5f534173af13
Park <parkia@amazon.com>
2018-06-15 17:22:59 +0100

    Modifying the publish script for AWS S3 integration

--- publish.sh
+++ publish.sh
@@ -1,1 +1,7 @@
-sudo cp -R css img jsx js output index.html /var/www/html/shc/
+python ./get_users.py
+DATE=`date +%Y-%m-%d`
+mkdir output/$DATE
+python abuser.py --f user_list.txt -o output/$DATE/
+aws s3 cp output/$DATE/ s3://health.steeme.com/output/$DATE/ --recursive
+aws s3 cp output/metadata.json s3://health.steeme.com/output/
+
